4a6fbecf629d1943b6d22cb2833dbb9121ef01de
Khargol <josephklein3@gmail.com>
2020-06-20 18:32:13 -0400

    task adding working

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,21 @@ formEl.addEventListener("submit", createTaskHandler);
 
 function createTaskHandler(event){
     event.preventDefault();
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; //sets class in html
-    listItemEl.textContent = "This is a new task."; //sets text area between <li></li>
-    tasksToDoEl.appendChild(listItemEl); //adds to end of list    
+    let taskNameInput = document.querySelector("input[name='task-name']").value; //selects the input element with name attribute set to task-name
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);    
 }
