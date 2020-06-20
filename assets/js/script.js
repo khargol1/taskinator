53016602792cf9844c896a33faacb1851e40cdea
Khargol <josephklein3@gmail.com>
2020-06-20 18:15:03 -0400

    event handler on form submit working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //selects id from html
 
 /* event listner with nameless function
@@ -9,9 +9,10 @@ buttonEl.addEventListener("click", function() { //explicitly run the nameless fu
   tasksToDoEl.appendChild(listItemEl); //adds to end of list
 }); */
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
-function createTaskHandler(){
+function createTaskHandler(event){
+    event.preventDefault();
     var listItemEl = document.createElement("li"); 
     listItemEl.className = "task-item"; //sets class in html
     listItemEl.textContent = "This is a new task."; //sets text area between <li></li>
